db24261e6461ad1571daf04773355ca7a11a8a1d
mjbalcueva <75126648+mjbalcueva@users.noreply.github.com>
2025-08-28 13:02:08 +0800

    Refactor configuration files and improve code formatting: update ESLint and Prettier configurations, enhance TypeScript settings, and standardize JSON formatting across multiple files.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,60 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+	baseDirectory: __dirname
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
-];
+	{
+		ignores: ["postcss.config.mjs", "tailwind.config.ts"]
+	},
+	...compat.config({
+		parser: "@typescript-eslint/parser",
+		parserOptions: {
+			project: true
+		},
+		extends: [
+			"next/core-web-vitals",
+			"next/typescript",
+			"prettier",
+			"plugin:@typescript-eslint/recommended-type-checked",
+			"plugin:@typescript-eslint/stylistic-type-checked",
+			"plugin:@tanstack/query/recommended"
+		],
+		rules: {
+			"@typescript-eslint/array-type": "off",
+			"@typescript-eslint/consistent-type-definitions": "off",
+			"@typescript-eslint/consistent-type-imports": [
+				"warn",
+				{
+					prefer: "type-imports",
+					fixStyle: "inline-type-imports"
+				}
+			],
+			"@typescript-eslint/no-unused-vars": [
+				"warn",
+				{
+					argsIgnorePattern: "^_"
+				}
+			],
+			"@typescript-eslint/no-explicit-any": "warn",
+			"@typescript-eslint/require-await": "off",
+			"@typescript-eslint/no-misused-promises": [
+				"error",
+				{
+					checksVoidReturn: {
+						attributes: false
+					}
+				}
+			],
+			"no-unreachable": "warn"
+		}
+	})
+]
 
-export default eslintConfig;
+export default eslintConfig
---
+++ prettier.config.mjs
@@ -0,0 +1,35 @@
+/** @type {import('prettier').Config & import('@ianvs/prettier-plugin-sort-imports').PluginConfig & import('prettier-plugin-tailwindcss').PluginOptions} */
+const config = {
+	bracketSameLine: false,
+	endOfLine: "lf",
+	importOrder: [
+		"^(next/(.*)$)|^(next$)|^(react/(.*)$)|^(react$)", // Next.js and React.js modules
+		"<BUILTIN_MODULES>", // Node.js built-in modules
+		"<THIRD_PARTY_MODULES>", // NPM packages
+		"",
+		"^@/core/(.*)$",
+		"",
+		"^@/services/(.*)$",
+		"",
+		"^@/features/(.*)$",
+		"",
+		"^@/(.*)$",
+		"",
+		"^(?!.*[.]css$)[./].*$",
+		".css$"
+	],
+	importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+	plugins: [
+		"@ianvs/prettier-plugin-sort-imports",
+		"prettier-plugin-tailwindcss"
+	],
+	printWidth: 80,
+	quoteProps: "consistent",
+	semi: false,
+	singleQuote: false,
+	tabWidth: 2,
+	trailingComma: "none",
+	useTabs: true
+}
+
+export default config
