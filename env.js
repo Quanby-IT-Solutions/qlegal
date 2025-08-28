34fc5db7182c0085e750d2bbd18a43b1c0b357c7
mjbalcueva <75126648+mjbalcueva@users.noreply.github.com>
2025-08-28 20:29:21 +0800

    feat(env): add DATABASE_URL and SEED_VALUE to environment configuration; enhance ESLint with drizzle plugin and rules; update package.json with new database-related scripts and dependencies

--- env.js
+++ env.js
@@ -5,14 +5,18 @@ export const env = createEnv({
 	server: {
 		AUTH_SECRET: process.env.NODE_ENV === "production" ? z.string() : z.string().optional(),
 		AUTH_URL: z.string().optional(),
+		DATABASE_URL: z.string().min(1),
 		NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
+		SEED_VALUE: z.coerce.number().optional(),
 		PORT: process.env.PORT ? z.coerce.number() : z.coerce.number().default(3000),
 	},
 	client: {},
 	runtimeEnv: {
 		AUTH_SECRET: process.env.AUTH_SECRET,
 		AUTH_URL: process.env.AUTH_URL,
+		DATABASE_URL: process.env.DATABASE_URL,
 		NODE_ENV: process.env.NODE_ENV,
+		SEED_VALUE: process.env.SEED_VALUE,
 		PORT: process.env.PORT,
 	},
 	skipValidation: !!process.env.SKIP_ENV_VALIDATION,
