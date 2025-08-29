8bc4c6670d8bb34eb33038ee05e23a4638a02d9b
mjbalcueva <75126648+mjbalcueva@users.noreply.github.com>
2025-08-29 10:06:45 +0800

    feat(pages): add new pages for departments, documents, envelopes, and organizations with specific and update views

---
+++ app/(site)/(department)/department/update/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+	return (
+		<div>
+			<h1>Update Specific Department</h1>
+		</div>
+	)
+}
---
+++ app/(site)/(document)/document/(view)/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+	return (
+		<div>
+			<h1>Specific Document</h1>
+		</div>
+	)
+}
