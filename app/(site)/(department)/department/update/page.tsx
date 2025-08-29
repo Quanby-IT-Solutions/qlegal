5c20efc39ca09a6ace5daff19e224018a964f93a
chano <christianmontesor@gmail.com>
2025-08-29 10:36:16 +0800

    chore(pages): remove obsolete department and organization pages to streamline the application structure

--- app/(site)/(department)/department/update/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-	return (
-		<div>
-			<h1>Update Specific Department</h1>
-		</div>
-	)
-}
--- app/(site)/(department)/departments/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-	return (
-		<div>
-			<h1>All Departments</h1>
-		</div>
-	)
-}
