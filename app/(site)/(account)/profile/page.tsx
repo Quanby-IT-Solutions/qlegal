ab4e564ababd9cfe82fbc53361453b859384727e
mjbalcueva <75126648+mjbalcueva@users.noreply.github.com>
2025-09-10 13:52:07 +0800

    feat: implement ShapesContainer component to enhance profile page visuals

--- app/(site)/(account)/profile/page.tsx
+++ app/(site)/(account)/profile/page.tsx
@@ -1,7 +1,14 @@
+import { Navbar } from "@/features/home/components/navbar"
+import ShapesContainer from "@/features/profile/components/shapes-container"
+
 export default function Page() {
 	return (
-		<div>
-			<h1>Profile page</h1>
+		<div className="from-background via-background to-muted/20 flex flex-col bg-gradient-to-br">
+			<Navbar isAuthenticated={true} />
+
+			<ShapesContainer>
+				<h1>Profile page</h1>
+			</ShapesContainer>
 		</div>
 	)
 }
