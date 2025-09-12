23d228eb524393620a34f0f47a1b0ad86e44f13d
mjbalcueva <75126648+mjbalcueva@users.noreply.github.com>
2025-09-12 22:43:33 +0800

    fix: adjust styling of Two-Factor Authentication form item
    - Removed shadow from the `FormItem` component in the `ToggleTwoFAForm` for a cleaner appearance.
    - Maintained overall layout structure while enhancing visual consistency.

--- features/settings/components/forms/form.toggle-two-fa.tsx
+++ features/settings/components/forms/form.toggle-two-fa.tsx
@@ -57,7 +57,7 @@ export function ToggleTwoFAForm() {
 					control={form.control}
 					name="enabled"
 					render={({ field }) => (
-						<FormItem className="flex flex-row items-center justify-between rounded-sm border p-4 shadow-sm">
+						<FormItem className="flex flex-row items-center justify-between rounded-sm border p-4">
 							<div className="space-y-0.5">
 								<FormLabel>Two-Factor Authentication</FormLabel>
 								<FormDescription>
