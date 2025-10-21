1d216dc6eae4832d03efaf20c93ff5b2ab3ce039
mjbalcueva <75126648+mjbalcueva@users.noreply.github.com>
2025-10-21 10:57:41 +0800

    refactor: remove unused clean script and update seed and reset scripts

--- services/drizzle/reset.ts
+++ services/drizzle/reset.ts
@@ -6,7 +6,7 @@ import { db } from "@/services/drizzle/db"
 import { schema } from "@/services/drizzle/schema"
 
 async function main() {
-	await reset(db, { schema })
+	await reset(db, schema)
 	await db.$client.end()
 }
 
