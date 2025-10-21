d23aa0afc7730fa7f24fdf189a2efebfeb4aa482
mjbalcueva <75126648+mjbalcueva@users.noreply.github.com>
2025-10-21 10:57:41 +0800

    refactor: remove unused clean script and update seed and reset scripts

--- services/drizzle/seed.ts
+++ services/drizzle/seed.ts
@@ -1,14 +1,9 @@
 import "dotenv/config"
 
-import { reset } from "drizzle-seed"
-
 import { db } from "@/services/drizzle/db"
-import * as schema from "@/services/drizzle/schema"
 import { createUsers } from "@/services/drizzle/seed/users"
 
 async function main() {
-	await db.$client.query("SELECT 1")
-	await reset(db, schema)
 	await createUsers()
 	await db.$client.end()
 }
