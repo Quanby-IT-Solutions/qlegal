c0a50a59d4f35d376d07e627f1ecb1db2b63819b
Jude Besmonte <juddebesmonte@gmail.com>
2025-10-28 13:50:43 +0800

    fix: remove async keyword from FindALawyerPage function

--- app/(site)/find-a-lawyer/page.tsx
+++ app/(site)/find-a-lawyer/page.tsx
@@ -2,7 +2,7 @@ import { SiteNavbar } from "@/core/components/navbar/site-navbar"
 
 import { LawyersPage } from "@/features/lawyers/components/lawyers-page"
 
-export default async function FindALawyerPage() {
+export default function FindALawyerPage() {
 	return (
 		<>
 			<SiteNavbar items={[{ label: "Find a Lawyer", url: "/find-a-lawyer" }]} />
