032ac148405623de28dd228e3b3fd206b7f1cf86
Jude Besmonte <juddebesmonte@gmail.com>
2025-11-13 11:07:36 +0800

    refactor(layout): remove Navbar component from Layout for cleaner structure

--- app/(site)/(account)/layout.tsx
+++ app/(site)/(account)/layout.tsx
@@ -1,9 +1,6 @@
-import { Navbar } from "@/features/home/components/navbar"
-
 export default function Layout(props: LayoutProps<"/">) {
 	return (
 		<div className="min-h-screen">
-			<Navbar />
 			{props.children}
 		</div>
 	)
