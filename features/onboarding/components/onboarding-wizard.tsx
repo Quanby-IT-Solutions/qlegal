77aff6f883e262f0a51e4a93fbead5edd3ee1f4c
mjbalcueva <75126648+mjbalcueva@users.noreply.github.com>
2026-03-11 08:28:09 +0800

    feat(onboarding): enhance onboarding wizard with logout functionality and improved button layout
    - Added a logout button to the onboarding wizard, allowing users to log out easily.
    - Introduced icons for the logout and get started buttons to improve visual feedback.
    - Updated the layout of buttons in the card footer for better accessibility and user experience.

--- features/onboarding/components/onboarding-wizard.tsx
+++ features/onboarding/components/onboarding-wizard.tsx
@@ -1,10 +1,13 @@
 "use client"
 
 import { useState } from "react"
+import { CircleArrowRight, Logout01Icon } from "@hugeicons/core-free-icons"
+import { HugeiconsIcon } from "@hugeicons/react"
 
 import { QuanbyLogo } from "@/core/components/quanby-logo"
 import { Button } from "@/core/components/ui/button"
-import { Card, CardDescription, CardHeader, CardTitle } from "@/core/components/ui/card"
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/core/components/ui/card"
+import { handleLogout } from "@/core/lib/auth.utils"
 import { cn } from "@/core/lib/utils"
 
 import { OnboardingWizardContent } from "./onboarding-wizzard-content"
@@ -16,6 +19,12 @@ export function OnboardingWizard() {
 	const handleRestartWelcome = () => {
 		setIsExpanded(false)
 		setHasStarted(false)
+	}
+
+	const handleLogoutClick = () => {
+		setIsExpanded(false)
+		setHasStarted(false)
+		void handleLogout("/login")
 	}
 
 	return (
@@ -30,14 +39,22 @@ export function OnboardingWizard() {
 					<div className="mb-2 flex justify-center">
 						<QuanbyLogo className="size-14" />
 					</div>
-					<CardTitle className="text-2xl">Account Onboarding</CardTitle>
+					<CardTitle className="text-xl">Account Onboarding</CardTitle>
 					<CardDescription>
 						Let&apos;s set up your account. We&apos;ll walk you through a few quick steps to secure
 						your account and personalise your profile.
 					</CardDescription>
-					<Button onClick={handleStart} className="mt-8 w-full" size="lg">
-						Get started
-					</Button>
+
+					<CardFooter className="mt-8 flex justify-between px-0!">
+						<Button onClick={handleLogoutClick} size="sm" variant="ghost">
+							<HugeiconsIcon icon={Logout01Icon} />
+							Log out
+						</Button>
+						<Button onClick={handleStart} size="sm">
+							<HugeiconsIcon icon={CircleArrowRight} />
+							Get started
+						</Button>
+					</CardFooter>
 				</CardHeader>
 			)}
 
