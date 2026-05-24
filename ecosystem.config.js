2a9b2c13d338df58f87ec0b055bfc667d88f411c
mjbalcueva <75126648+mjbalcueva@users.noreply.github.com>
2026-05-24 07:53:13 +0000

    fix: update ecosystem configuration by adding missing properties for improved process management

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,9 +4,13 @@ module.exports = {
       name: "qsign-main",
       script: "pnpm",
       args: "start",
+      cwd: "/home/ubuntu/qlegal",
+      kill_timeout: 5000,
+      restart_delay: 3000,
+      max_restarts: 10,
       env: {
-          PORT: 3000
-      }
+        PORT: 3000,
+      },
     },
-  ]
+  ],
 };
